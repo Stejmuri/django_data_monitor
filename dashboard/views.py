c8f8b2221b54c7e68b3cee00117af7b62c940eea
Stejmuri <138825475+Stejmuri@users.noreply.github.com>
2025-08-04 14:42:59 -0500

    Update

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    """
-    Render the dashboard index page.
-    """
-    return render(request, 'dashboard/base.html')
+    data = {
+        'title': "Landing Page' Dashboard",
+    }
+
+    return render(request, 'dashboard/index.html', data)
