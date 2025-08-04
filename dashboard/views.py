ba31d00610ef6cc6246d61650a703f56e71d1874
Stejmuri <138825475+Stejmuri@users.noreply.github.com>
2025-08-04 14:11:44 -0500

    Update

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    """
+    Render the dashboard index page.
+    """
+    return render(request, 'dashboard/base.html')
